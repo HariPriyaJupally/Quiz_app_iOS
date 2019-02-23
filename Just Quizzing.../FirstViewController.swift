70c17fd200bc5f1c7bb785177b7c0b5b229ea6fe
student <student@nw45516.nwmissouri.edu>
2019-02-23 15:57:51 -0600

    Initial Commit

---
+++ Just Quizzing.../FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Just Quizzing...
+//
+//  Created by student on 2/23/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Just Quizzing.../SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Just Quizzing...
+//
+//  Created by student on 2/23/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
