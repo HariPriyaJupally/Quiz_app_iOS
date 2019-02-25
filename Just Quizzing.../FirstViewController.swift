79cf704614a9d5db96a44642a63f42ce4ecb8d1f
student <student@nw45516.nwmissouri.edu>
2019-02-24 20:33:03 -0600

    Initial Commit

--- Just Quizzing.../FirstViewController.swift
+++ Just Quizzing.../FirstViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var pickerDifficulty: UIPickerView!
+    
+    @IBOutlet weak var pickerType: UIPickerView!
+    
+    var pickerDifficultyContents: [String] = [String]()
+    
+    var pickerTypeContents: [String] = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = #colorLiteral(red: 0.9683927796, green: 0.8877285699, blue: 0.9686274529, alpha: 1)
+        
+        self.pickerDifficulty.delegate = self
+        self.pickerDifficulty.dataSource = self
+        
+        self.pickerType.delegate = self
+        self.pickerType.dataSource = self
+        pickerDifficulty.tag = 1
+        pickerType.tag = 2
+        pickerDifficultyContents = ["Easy", "Medium", "Difficult"]
+        
+        pickerTypeContents = ["Multiple Choice", "True or False", "Both"]
+        
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1{
+        return pickerDifficultyContents.count
+        }else{
+            return pickerTypeContents.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 1{
+        return pickerDifficultyContents[row]
+        }else{
+            return pickerTypeContents[row]
+        }
     }
 
 
