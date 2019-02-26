85173f94685fe9cc50a134261803fc04d8c408e7
hyndavi reddy <hyndavi.musipatla1996@gmail.com>
2019-02-25 22:04:52 -0600

    Modified History view controller

--- Just Quizzing.../HistoryViewController.swift
+++ Just Quizzing.../HistoryViewController.swift
@@ -1,36 +1,37 @@
 //
-//  SecondViewController.swift
+//  ViewController.swift
 //  Just Quizzing...
 //
-//  Created by student on 2/23/19.
+//  Created by Shruthi  Patlolla on 2/25/19.
 //  Copyright © 2019 student. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    var players = ["Hari Priya", "Hyndavi", "Sai Ram", "Indra"]
-    var ranks = [1,2,3,4]
+class HistoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var history = ["Hari Priya", "Indra"]
+    
+    var scores = [1,2,3,4]
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 0 {
-            return players.count
+            return history.count
         }  else {
             return -1
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "players")!
-        cell.textLabel?.text = players[indexPath.row]
-        cell.detailTextLabel?.text = String(ranks[indexPath.row])
+        let cell = tableView.dequeueReusableCell(withIdentifier: "histories")!
+        cell.textLabel?.text = history[indexPath.row]
+        cell.detailTextLabel?.text = String(scores[indexPath.row])
         return cell
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
     
     
 }
