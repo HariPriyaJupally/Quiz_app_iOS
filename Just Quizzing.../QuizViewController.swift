9f98aba0233b789d9d2df9ab4d13c8bd78dc9ebd
badisasairam <jharipriya@outlook.com>
2019-03-11 17:24:53 -0500

    Added QuizViewController by SaiRam Badisa

---
+++ Just Quizzing.../QuizViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuizViewController.swift
+//  Just Quizzing...
+//
+//  Created by Student on 3/11/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
