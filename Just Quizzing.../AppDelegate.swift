c52cebc85a8f114107bdf92c7f0b9db7ca960890
Indra Reddy <S533981@nwmissouri.edu>
2019-04-04 23:21:57 -0500

    decreased sleep value

--- Just Quizzing.../AppDelegate.swift
+++ Just Quizzing.../AppDelegate.swift
@@ -49,7 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        sleep(50)
+        sleep(20)
         
         return true
     }
